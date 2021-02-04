0dafb9e8d18e2ba3e4ed70245901695b93f9c45f
ynorena <ynorena@consensussa.com>
2021-02-03 22:15:28 -0500

    se maqueto menu usuario, menu admistrador

--- app/src/main/java/com/appcontrol/appcontroledu/LoginActivity.java
+++ app/src/main/java/com/appcontrol/appcontroledu/LoginActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
@@ -14,12 +15,21 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         getSupportActionBar().hide();
+        final Button btn_iniciar = (Button) findViewById(R.id.btn_iniciar);
         final TextView tvSign_up = (TextView) findViewById(R.id.sign_up);
         //metodo para llamar el formulario de perfil registro
         tvSign_up.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent myIntent = new Intent(LoginActivity.this, RegistroPerfilActivity.class);
+                startActivity(myIntent);
+            }
+        });
+
+        btn_iniciar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(LoginActivity.this, MenuPrincipalUserActivity.class);
                 startActivity(myIntent);
             }
         });
--- app/src/main/java/com/appcontrol/appcontroledu/RegistroUsuarioActivity.java
+++ app/src/main/java/com/appcontrol/appcontroledu/RegistroUsuarioActivity.java
@@ -1,8 +1,18 @@
 package com.appcontrol.appcontroledu;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
 
 public class RegistroUsuarioActivity extends AppCompatActivity {
 
@@ -10,5 +20,45 @@ public class RegistroUsuarioActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_usuario);
+
+        EditText et_fechaNacimiento = (EditText) findViewById(R.id.et_fechaNacimiento);
+        et_fechaNacimiento.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialogDatePickerDark((EditText) view);
+
+                            }
+        });
+
+    }
+
+    private void dialogDatePickerDark(final EditText bt) {
+        Calendar cur_calender = Calendar.getInstance();
+        DatePickerDialog datePicker = DatePickerDialog.newInstance(
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
+                        Calendar calendar = Calendar.getInstance();
+                        calendar.set(Calendar.YEAR, year);
+                        calendar.set(Calendar.MONTH, monthOfYear);
+                        calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                        long date_ship_millis = calendar.getTimeInMillis();
+                        ((TextView) findViewById(R.id.et_fechaNacimiento)).setText(getFormattedDateSimple(date_ship_millis));
+                    }
+                },
+                cur_calender.get(Calendar.YEAR),
+                cur_calender.get(Calendar.MONTH),
+                cur_calender.get(Calendar.DAY_OF_MONTH)
+        );
+        //set dark theme
+        datePicker.setThemeDark(true);
+        datePicker.setAccentColor(getResources().getColor(R.color.colorPrimary));
+        datePicker.show(getFragmentManager(), "Datepickerdialog");
+    }
+
+
+    public static String getFormattedDateSimple(Long dateTime) {
+        SimpleDateFormat newFormat = new SimpleDateFormat("MMMM dd, yyyy");
+        return newFormat.format(new Date(dateTime));
     }
 }
