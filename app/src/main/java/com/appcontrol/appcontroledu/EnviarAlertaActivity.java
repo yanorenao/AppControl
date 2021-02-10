9311813f0e22962653519495bed16bb7b9fad496
ynorena <ynorena@consensussa.com>
2021-02-10 10:29:23 -0500

    se maqueta formulario de reportar alerta

---
+++ app/src/main/java/com/appcontrol/appcontroledu/EnviarAlertaActivity.java
@@ -0,0 +1,14 @@
+package com.appcontrol.appcontroledu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EnviarAlertaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enviar_alerta);
+    }
+}
