ecb5f21a5e42eb7a81dfc803212e8ee207997ba0
ynorena <ynorena@consensussa.com>
2021-02-15 22:19:49 -0500

    navegacion app usuario

--- app/src/main/java/com/appcontrol/appcontroledu/activity/MenuPrincipalUserActivity.java
+++ app/src/main/java/com/appcontrol/appcontroledu/activity/MenuPrincipalUserActivity.java
@@ -1,16 +1,44 @@
 package com.appcontrol.appcontroledu.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.appcontrol.appcontroledu.R;
 
 public class MenuPrincipalUserActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal_user);
+        getSupportActionBar().hide();
+        CardView cardView_reportarSalud = (CardView) findViewById(R.id.reportar_salud);
+        CardView cardView_modificaPerfil = (CardView) findViewById(R.id.modificar_perfil);
+
+
+        cardView_reportarSalud.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(MenuPrincipalUserActivity.this, ReporteSaludActivity.class);
+                startActivity(myIntent);
+            }
+        });
+
+        cardView_modificaPerfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(MenuPrincipalUserActivity.this, RegistroUsuarioActivity.class);
+                startActivity(myIntent);
+
+            }
+        });
+
     }
 }
--- app/src/main/java/com/appcontrol/appcontroledu/activity/RegistroPerfilActivity.java
+++ app/src/main/java/com/appcontrol/appcontroledu/activity/RegistroPerfilActivity.java
@@ -2,7 +2,10 @@ package com.appcontrol.appcontroledu.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.appcontrol.appcontroledu.R;
 
@@ -12,5 +15,52 @@ public class RegistroPerfilActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_perfil);
+
+        final Button btn_estudiante = (Button) findViewById(R.id.btn_estudiante);
+        final Button btn_docente = (Button) findViewById(R.id.btn_docente);
+        final Button btn_administrativo = (Button) findViewById(R.id.btn_administrativo);
+        final Button btn_contratista = (Button) findViewById(R.id.btn_contratista);
+        final Button btn_otro = (Button) findViewById(R.id.btn_otro);
+
+        btn_estudiante.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(RegistroPerfilActivity.this, RegistroUsuarioActivity.class);
+                startActivity(myIntent);
+            }
+        });
+
+        btn_administrativo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(RegistroPerfilActivity.this, RegistroUsuarioActivity.class);
+                startActivity(myIntent);
+            }
+        });
+
+        btn_docente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(RegistroPerfilActivity.this, RegistroUsuarioActivity.class);
+                startActivity(myIntent);
+            }
+        });
+
+        btn_contratista.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(RegistroPerfilActivity.this, RegistroUsuarioActivity.class);
+                startActivity(myIntent);
+            }
+        });
+
+        btn_otro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(RegistroPerfilActivity.this, RegistroUsuarioActivity.class);
+                startActivity(myIntent);
+            }
+        });
+
     }
 }
